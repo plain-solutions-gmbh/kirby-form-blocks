3b0cf1ed52bbba220f92098864559fe680358e7a
René Stalder <mail@renestalder.me>
2022-01-07 17:48:33 +0100

    Wrap radio and checkbox groups as fieldset with legen tag
    Improve HTML markup semantics by using fieldset tag and the legen tag
    to group multiple controls like radio buttons and checkboxes.

--- snippets/blocks/form.php
+++ snippets/blocks/form.php
@@ -1,33 +1,27 @@
 <?php if ($form->showForm()) : ?>
-
     <form method="post" id="<?= $form->id() ?>" action="<?= $page->url() . "#" . $form->id() ?>" novalidate>
 
         <div class="form-block" style="display:grid;grid-gap: 1em 0.5em; grid-template-columns: repeat(12, 1fr);">
 
             <?php foreach ($form->fields() as $field) : ?>
-                <div class="form-block-field form-block-field-<?= $field->type(true) ?>" style="grid-column: span <?= $field->width('grid') ?>" data-id="<?= $field->slug() ?>">
-                    <?php if ($field->type() === 'formfields/radio' || $field->type() === 'formfields/checkbox') : ?>
-                        <p for="<?= $field->slug() ?>" class="form-block-field-label">
-                            <span class="form-block-field-label-text"><?= $field->label() ?></span>
-                            <?php if ($field->required()->isTrue()) : ?>
-                                <span class="form-block-field-label-required" aria-hidden="true">*</span>
-                            <?php endif ?>
-                        </p>
-                    <?php else : ?>
-                        <label for="<?= $field->slug() ?>" class="form-block-field-label">
-                            <span class="form-block-field-label-text"><?= $field->label() ?></span>
-                            <?php if ($field->required()->isTrue()) : ?>
-                                <span class="form-block-field-label-required" aria-hidden="true">*</span>
-                            <?php endif ?>
-                        </label>
-                    <?php endif ?>
+                <?php $isFieldGroup = ($field->type() === 'formfields/radio' || $field->type() === 'formfields/checkbox') ?>
+                <?php $fieldTag = $isFieldGroup ? 'fieldset' : 'div' ?>
+                <?php $fieldGroupLabelTag = $isFieldGroup ? 'legend' : 'label' ?>
+                <<?= $fieldTag ?> class="form-block-field form-block-field-<?= $field->type(true) ?>" style="grid-column: span <?= $field->width('grid') ?>" data-id="<?= $field->slug() ?>">
+                    <<?= $fieldGroupLabelTag ?> for="<?= $field->slug() ?>" class="form-block-field-label">
+                        <span class="form-block-field-label-text"><?= $field->label() ?></span>
+                        <?php if ($field->required()->isTrue()) : ?>
+                            <span class="form-block-field-label-required" aria-hidden="true">*</span>
+                        <?php endif ?>
+                    </<?= $fieldGroupLabelTag ?>>
+
 
                     <?php if (!$field->isValid()) : ?>
                         <span id="<?= $field->slug() ?>-error-message" class="form-block-message form-block-field-invalid"><?= $field->errorMessage() ?></span>
                     <?php endif ?>
 
                     <?= $field->toHtml() ?>
-                </div>
+                </<?= $fieldTag ?>>
             <?php endforeach ?>
 
             <?php if (!$form->isValid()) : ?>
